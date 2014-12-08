0a26a42cbaffeb54be6e5e4673ba8095d279e35f
Tim <Tim Eulitz>
2014-12-08 15:24:57 +0100

    Added AJAX request to Max' awesome and really well-designed website to grab the list of troublemakers

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
 $(document).ready(function() {
     drawmap();
+    getJsonData();
 });
+
+function getJsonData() {
+    $.ajax({
+        url:"http://www.maxbehr.de/opendata/dataset.php",
+        dataType: 'jsonp',
+        success:function(json){
+            alert("Success");
+        },
+        error:function(){
+            alert("Error");
+        }
+    });
+}
 
 
