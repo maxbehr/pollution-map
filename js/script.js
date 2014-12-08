811b8d02da4d63b2f818f791ef5013d200029bdb
max <hello@maxbehr.de>
2014-12-08 15:00:01 +0100

    Grap json file!

--- js/script.js
+++ js/script.js
@@ -1,5 +1,31 @@
 $(document).ready(function() {
     drawmap();
+
+	var request = $.getJSON( "./data/dataset.json", function( data ) {
+		console.log( "success" );
+	})
+	.done(function( data ) {
+		console.log( "second success" );
+
+		//var stoffname = "Kohlendioxid (CO2)";
+		var results = [];
+
+		$.each(data, function(index, element) {
+
+			//if(element.stoff_name === stoffname) {
+			//	results.push(element);
+			//}
+
+		});
+
+		console.log(results.length);
+	})
+	.fail(function(jqXHR, textStatus ) {
+		console.log( textStatus );
+	})
+	.always(function() {
+		console.log( "complete" );
+	});
 });
 
 
