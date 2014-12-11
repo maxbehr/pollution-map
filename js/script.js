58d65b6b46f1c3b24bcdccae896f7e98ea2b9341
max <hello@maxbehr.de>
2014-12-11 23:47:23 +0100

    After all, I had to add some fanciness to that project.

--- js/script.js
+++ js/script.js
@@ -1,17 +1,53 @@
 $(document).ready(function() {
     drawmap();
+    getJsonData();
 
-function getJsonData() {
-    $.ajax({
-        url:"http://www.maxbehr.de/opendata/dataset.php",
-        dataType: 'jsonp',
-        success:function(json){
-            alert("Success");
-        },
-        error:function(){
-            alert("Error");
-        }
-    });
-}
+	function getJsonData() {
+	    var request = $.ajax({
+			url: "http://www.maxbehr.de/opendata/dataset.php?callback=?",
+			dataType: "jsonp",
+			success: function( data ) {
+
+
+				//console.log(data);
+
+				/*
+				 *	Filter by: Stoffname
+				 *
+				 */
+
+				var stoffname = "Amoniak (NH3)";
+				var results = [];
+
+				$.each(data, function(index_element, element) {
+
+					addMarker( element.wgs84_x, element.wgs84_y );
+					var elem = createResult( element.name, element.bundesland );
+					$('#results').append(elem);
+
+					$.each( element.fracht, function( index_stoff, stoff ) {
+
+						if(stoff.stoff_name == stoffname) {
+
+							console.log("jep");
+							//results.push(element);
+
+						}
+
+					});
+
+				});
+
+			}
+		});
+	}
+
+	function createResult( name, bundesland ) {
+
+		var elem = '<li><h1>'+ name +'</h1><table><tr> <td class="key">Bundesland</td> <td>'+ bundesland +'</td> </tr> </table> </li>';
+		return elem;
+
+	}
+});
 
 
