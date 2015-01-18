a8a40ce18470a1bb44af243254c3d833999b3b3e
Tim <Tim Eulitz>
2015-01-18 14:28:48 +0100

    First of all, Felix, screw you for spying. Secondly, I have fixed lots of the shitty shit that Max did and implemented the leaflet markers, however, for some reason their content is invisible at the moment, gotta debug that later.

--- js/script.js
+++ js/script.js
@@ -1,56 +1,85 @@
-$(document).ready(function() {
+$(document).ready(function () {
     drawmap();
     getJsonData();
 
-	function getJsonData() {
-	    var request = $.ajax({
-			url: "http://www.maxbehr.de/opendata/dataset.php?callback=?",
-			dataType: "jsonp",
+    function getJsonData() {
+        var request = $.ajax({
+            url: "http://www.maxbehr.de/opendata/dataset.php?callback=?",
+            dataType: "jsonp",
             data: {
                 limit: 100
             },
-			success: function( data ) {
+            success: function (data) {
+                $.each(data, function (index_element, element) {
+                    results.push(element);
+                });
+                setTimeout(function() {updateElements();}, 500);
+            }
+        });
+    }
+
+    function updateElements(){
+        var resultBlock =  $('#results');
+        markers = [];
+
+        resultBlock.fadeOut("slow", function() {
+            resultBlock.empty();
+            $.each(results, function (index_element, element) {
+                var currentMarker = L.marker([element.wgs84_y, element.wgs84_x]);
+                currentMarker.addTo(map);
+                markers[element.id] = currentMarker;
+
+                var elem = createResult(element);
+                resultBlock.append(elem);
+            });
+            $('#results').fadeIn("slow");
 
 
-				//console.log(data);
+            $("#results li").on( "mouseenter", function(event) {
+                    hoverId = $(this).attr('id');
+                    if (!clickedMarker) {
+                        markers[hoverId].bindPopup("<b>Hello world!</b><br>I am a popup."); //TODO: Remove this part here, popup content needs to be assigned somewhere else, probably when the marker is set.
+                        markers[hoverId].openPopup();
+                        $("#results #" + hoverId).addClass('active');
+                    }
+            });
 
-				/*
-				 *	Filter by: Stoffname
-				 *
-				 */
+            $("#results li").on( "mouseleave", function(event) {
+                if (!clickedMarker) {
+                    markers[hoverId].closePopup();
+                    $("#results #" + hoverId).removeClass('active');
+                    hoverId = null;
+                }
+            });
 
-				var stoffname = "Amoniak (NH3)";
-				var results = [];
+            $("#results li a").on( "click", function(event) {
+                event.stopPropagation();
+                var parentId = $(this).parent().attr('id')
+                if (parentId == clickedMarker) {
+                    clickedMarker = null;
+                } else {
+                    if (clickedMarker) {
+                        //markers[clickedMarker].closePopup();
+                        $("#results #" + clickedMarker).removeClass('active');
+                    }
+                    markers[hoverId].bindPopup("<b>Hello world!</b><br>I am a popup."); //TODO: Remove this part here, popup content needs to be assigned somewhere else, probably when the marker is set.
+                    markers[parentId].openPopup();
+                    $("#results #" + parentId).addClass('active');
+                    clickedMarker = parentId;
+                }
+            });
 
-				$.each(data, function(index_element, element) {
+            map.on('popupclose', function(e) {
+                clickedMarker = null;
+                $("#results li").removeClass('active');
+            });
 
-					addMarker( element.wgs84_x, element.wgs84_y );
-					var elem = createResult( element.name, element.bundesland );
-					$('#results').append(elem);
+        });
+    }
 
-					$.each( element.fracht, function( index_stoff, stoff ) {
-
-						if(stoff.stoff_name == stoffname) {
-
-							console.log("jep");
-							//results.push(element);
-
-						}
-
-					});
-
-				});
-
-			}
-		});
-	}
-
-	function createResult( name, bundesland ) {
-
-		var elem = '<li><h1>'+ name +'</h1><table><tr> <td class="key">Bundesland</td> <td>'+ bundesland +'</td> </tr> </table> </li>';
-		return elem;
-
-	}
+    function createResult(element) {
+        return '<li id="' + element.id + '"><a href="#"><h1>' + element.name + '</h1></a><table><tr> <td class="key">Bundesland:</td> <td>' + element.bundesland + '</td> </tr> </table> </li>';
+    }
 });
 
 
